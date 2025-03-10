c86284cd71838b2210eb3ffd4251f508b46b552e
Salil Saurav <salilsaurav.work@gmail.com>
2025-03-10 15:09:50 +0530

    theme refactored

--- components/utility/breadcrumb.php
+++ components/utility/breadcrumb.php
@@ -4,29 +4,46 @@ if (!defined('ABSPATH')) exit;
 
 function custom_breadcrumb_shortcode()
 {
-    // Initialize output
-    $output = '<nav class="breadcrumb">';
-    $output .= '<ol class="breadcrumb-list">';
-
-    // Add home link
-    $output .= '<li class="breadcrumb-item"><a href="' . home_url() . '">Home</a></li>';
-
-    // If not on home page, add current page/post info
-    if (!is_front_page()) {
-        if (is_category() || is_single()) {
-            $output .= '<li class="breadcrumb-item">' . get_the_category()[0]->name . '</li>';
-
-            if (is_single()) {
-                $output .= '<li class="breadcrumb-item">' . get_the_title() . '</li>';
-            }
-        } elseif (is_page()) {
-            $output .= '<li class="breadcrumb-item">' . get_the_title() . '</li>';
-        }
+    if (is_front_page()) {
+        return build_breadcrumb_html(['Home']);
     }
 
-    $output .= '</ol>';
-    $output .= '</nav>';
+    $breadcrumb_items = [
+        ['title' => 'Home', 'url' => home_url()]
+    ];
 
+    if (is_category() || is_single()) {
+        $category = get_the_category();
+        if (!empty($category)) {
+            $breadcrumb_items[] = ['title' => $category[0]->name];
+        }
+
+        if (is_single()) {
+            $breadcrumb_items[] = ['title' => get_the_title()];
+        }
+    } elseif (is_page()) {
+        $breadcrumb_items[] = ['title' => get_the_title()];
+    }
+
+    return build_breadcrumb_html($breadcrumb_items);
+}
+
+function build_breadcrumb_html($items)
+{
+    $output = '<nav class="breadcrumb"><ol class="breadcrumb-list">';
+
+    foreach ($items as $index => $item) {
+        $output .= '<li class="breadcrumb-item">';
+        if (isset($item['url'])) {
+            $output .= '<a href="' . esc_url($item['url']) . '">' . esc_html($item['title']) . '</a>';
+        } else {
+            $output .= esc_html($item['title']);
+        }
+        $output .= '</li>';
+    }
+
+    $output .= '</ol></nav>';
     return $output;
 }
+
 add_shortcode('breadcrumb', 'custom_breadcrumb_shortcode');
